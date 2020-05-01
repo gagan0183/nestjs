e8726e7abf58931a38a1ab2f2405ad10bd1a2e8c
Gagandeep singh <g.deepsingh1@gmail.com>
2020-05-01 14:12:07 +0530

    api for put course

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Put, Param, Body } from "@nestjs/common";
 import { Course } from '../../../../shared/course';
 import { CoursesRepository } from "../repositories/courses.repository";
 
@@ -10,5 +10,10 @@ export class CoursesController {
     @Get('/api/courses')
     async findAllCourses(): Promise<Course[]> {
         return this.coursedb.findAll();
+    }
+
+    @Put('/api/courses/:courseId')
+    async updateCourse(@Param('courseId') courseId: string, @Body() changes: Partial<Course>) {
+        console.log('update course');
     }
 }
